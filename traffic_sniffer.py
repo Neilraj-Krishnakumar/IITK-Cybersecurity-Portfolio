475010939a72af34e0a551214c51d22cb3896c9c
Neilraj Krishnakumar <vijayalaxmikrishnakumarkrishna@gmail.com>
2026-06-25 05:39:14 +0530

    Updates Project 2 code and screenshot

--- traffic_sniffer.py
+++ traffic_sniffer.py
@@ -2,10 +2,13 @@ import sys
 import random
 import time
 
-def run_network_sensor() :
+def run_network_sensor():
+    print("========================================================================")
+    print("[BURP SUITE NODE]: Live Packet Interception Active")
+    print("[DEVSECOPS TOOLING]: Simulating corporate Log Training Streams")
+    print("====================================================================")
     print("SYSTEM PLATFORM NATIVE LAYER:", sys.platform)
-    print("SENSOR ONLINE TIMESTAMP:", time.strftime("%H:%M:%S"))
-    print("INITIALIZING PACKET SNIFFER PROTOCOL")
+    print("SENSOR ONLINE TIMESTAMP:",time.strftime("%H:%M:%S"))
     time.sleep(1)
 
     target_threat_vector = "192.168.1.50"
@@ -13,19 +16,28 @@ def run_network_sensor() :
 
     while total_packet_count < 5:
         total_packet_count = total_packet_count + 1
-        system_time_stamp = time.strftime("%H:%M:%S")
+        system_time_stamp = time.strftime("%H:%M:%S") 
 
         captured_ip_node = "192.168.1." + str(random.randint(48, 52))
-        print("FRAME ATTACHED: Source network address: " + captured_ip_node + " logged at " + system_time_stamp)
+        print(f"\n[BURP FRAME {total_packet_count}]: SRC: {captured_ip_node} logged at {system_time_stamp}")
+
         if captured_ip_node == target_threat_vector:
-            print("WARNING ENGINE: Malicious infrastructure block detected!")
-            print("FIREWALL EXECUTOR: Terminating active session conections instantly.")
+            print("WARNING ENGINE : Malicious infrastructure block detected!")
+            print("FIREWALL EXECUTOR : Terminating active session connections instantly.")
         else:
-            print("ROUTING REPORT: Packet signature verified as clean.")
+            print("ROUTING REPORT : Packet signature verified as clean.")
 
-        time.sleep(1)
+        time.sleep(0.5)
+    
+    print("-------------------------------------------------------------------------")
+    print("STATUS: Burp Suite telemetry capture cycles completed.")
+    print("Total system frames inspected: " + str(total_packet_count))
+    print("======================================================================")
 
-        print("FORENSIC NETWORK DATA EVALUATION ANALYSIS COMPLETE")
-        print("Total system frames inspected: " + str(total_packet_count))
-run_network_sensor()
+run_network_sensor()   
 
+
+
+
+
+   
