965d2a034b00e20dee50aea760631a80e132aa4c
Neilraj Krishnakumar <vijayalaxmikrishnakumarkrishna@gmail.com>
2026-06-25 18:37:46 +0530

    Updates Project 3 code and screenshot.

--- forensic_scraper.py
+++ forensic_scraper.py
@@ -3,31 +3,36 @@ import random
 import time
 
 def scan_log_database():
+    print("========================================================================")
+    print("[OWASP JUICE SHOP AUDITOR]: Post-Incident Forensic Scraper")
+    print("[DEVSECOPS INSIGHT]:Scanning System Environment Logs")
+    print("====================================================================")
     print("SYSTEM PLATFORM NATIVE LAYER:", sys.platform)
     print("TIMESTAMP:", time.strftime("%H:%M:%S"))
     print("STARTING SECURE FORENSIC DATABASE SCRAPE PROTOCOL")
     time.sleep(1)
 
-    target_leak_token = "COMPROMISED"
+    targ_leak_token = "COMPROMISED"
     records_processed = 0
 
     while records_processed < 5:
         records_processed = records_processed + 1
         system_timestamp = time.strftime("%H:%M:%S")
 
-        possible_states = ["CLEAN VERIFIED","CLEAN VERIFIED","COMPROMISED"]
+        possible_states = ["CLEAN VERIFIED","CLEAN VERIFIED", "COMPROMISED"]
         simulated_states = random.choice(possible_states)
-        print("PARSING DATA ROW" + str(records_processed) + "AT" + system_timestamp + " STATUS: " + simulated_states)
+        print(f"\n[OWASP PARSE ROW {records_processed}] AT {system_timestamp} | STATUS: {simulated_states}")
 
-        if simulated_states == target_leak_token:
+        if simulated_states == targ_leak_token:
             print("WARNING ENGINE: Leak identified inside record database matrix!")
             print("ACTION LOG: Extracting entry data for threat isolation.")
         else:
-            print("VERIFICATION ENGINE: Record structures matches zero breach signatures.")
+            print("VERIFICATION ENGINE: Record structures match zero breach signatures.")
         time.sleep(1)
 
-    print("AUTOMATED FORENSIC SEARCH CYCLES COMPLETED") 
-    print("Total log sectors successfully parser: " + str(records_processed))
-scan_log_database()   
+    print("---------------------------------------------------------------------------------------------")
+    print("AUTOMATED FORENSIC SEARCH CYCLES COMPLETED")
+    print("Total log sectors successfully parsed: " + str(records_processed))
+    print("=================================================================================")
 
-               
+scan_log_database()
