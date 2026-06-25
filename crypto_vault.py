5eb38a212b00b49dfcffb067b79156d1c11cee42
Neilraj Krishnakumar <vijayalaxmikrishnakumarkrishna@gmail.com>
2026-06-25 19:20:09 +0530

    Updates Project 4 code and screenshot.

--- crypto_vault.py
+++ crypto_vault.py
@@ -3,6 +3,10 @@ import random
 import time
 
 def execute_crypto_vault():
+    print("=============================================================================")
+    print("[CRYPTO VAULT SYSTEM]: Symmetric Key Encryption Active")
+    print("[DEVSECOPS PARAMETER]: Simulating Secure Data Transit")
+    print("===================================================================")
     print("SYSTEM PLATFORM NATIVE LAYER:", sys.platform)
     print("VAULT CRYPTO TIMESTAMP:", time.strftime("%H:%M:%S"))
     print("INITIALIZING NATIONAL CRYPTOGRAPHIC SECTOR VAULT")
@@ -17,14 +21,14 @@ def execute_crypto_vault():
 
         raw_data_token = random.randint(65, 90)
         secure_encrypted_node = raw_data_token + secret_cipher_key
+        print(f"\n[VAULT TRANSACTION {total_blocks_encrypted}] AT {system_timestamp}")
+        print(" [RAW DATA BLOCK INDICES]: " + str(raw_data_token))
+        print(" [CIPHER EXTRACT NODE LAB]: " + str(secure_encrypted_node))
+        time.sleep(0.5)
 
-        print("VAULT ENGINE TRANSACTION: RECORD LAYER " + str(total_blocks_encrypted) + "AT" + system_timestamp)
-        print("  [RAW DATA BLOCK INDICES]  " + str(raw_data_token))
-        print("  [CIPHER EXTRACT NODE LAB]  " + str(secure_encrypted_node))
-
-        time.sleep(1)
-
+    print("-------------------------------------------------------------------------")
     print("NATIONAL CYBER DATA ENCRYPTION CRYPTO CYCLE COMPLETE")
-    print("Total system segments safely locked: " + str(total_blocks_encrypted))
-execute_crypto_vault()
+    print("Total systems segments safely locked: " + str(total_blocks_encrypted))
+    print("==============================================================================")
 
+execute_crypto_vault()
